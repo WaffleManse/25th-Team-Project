6739d7ea8dd9a06db365326381bff95b30e4fc3a
TenChoU <fgd5463@gmail.com>
2025-05-08 15:44:51 +0900

    [feat] Basement work
    기본 골조 함수 선언
    달리기 미구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D rigidbody = null;
+
+    public float forwardSpeed = 5f;
+
+    public bool GodMode = false;
+    public bool IsDead = false;
     
 }
