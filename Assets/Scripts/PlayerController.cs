e46422f494623245d4c86a9d53d09ce56b87bf9c
TenChoU <fgd5463@gmail.com>
2025-05-09 12:07:48 +0900

    [Fix] Textrunning Update
    자동이동 구현에 따른 최신화 저장

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody2D rigidbody2D; // rigidbody2d 컴포넌트 선언
+    [SerializeField] private Rigidbody2D rigid; // rigidbody2d 컴포넌트 선언
 
 
     public float Speed; // 이동 속도
 
-    private Rigidbody2D rb; 
+
 
     private Vector2 moveVelocity; // 이동 방향 벡터
 
 
     public float Jump = 5f; // 점프 힘 
 
+    public bool IsJump = false; // 점프 t/f 확인
+
     public bool GodMode = false; // 신 모드
     public bool IsDead = false; // 죽음 상태
 
-
-
-
-
-
-    private void Start()
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>(); // rigidbody2d 컴포넌트 가져오기
+        rigid = GetComponent<Rigidbody2D>();
     }
+
+
 
     private void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); // 입력 값 가져오기
-        moveVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); // 이동 방향 벡터 설정
-        
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")); // 입력 값 가져오기
+        transform.position += new Vector3(1f, 0, 0);
+        Jump();
+
 
     }
 
@@ -41,6 +41,25 @@ public class Player : MonoBehaviour
     {
         moveVelocity = new Vector2(moveVelocity.x, moveVelocity.y); // 수평 이동 처리
     }
-    
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!IsJump)
+            {
+                IsJump = true;
+                rigid.AddForce(Vector2.up * Jump, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name.Equals("Player"))
+        {
+            IsJump = false;
+        }
+    }
 
 }
