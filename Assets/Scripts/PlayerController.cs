3fb123c4b3e1781fadcabd97d385b4cc9632b19b
TenChoU <fgd5463@gmail.com>
2025-05-09 10:06:44 +0900

    [Feat] Script Correction
    플레이어 컨트롤 수정
    팔로우 카메라 생성
    그라운드 루프 생성

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,13 @@ public class Player : MonoBehaviour
 {
     Rigidbody2D rigidbody2D; // rigidbody2d 컴포넌트 선언
 
-    
-    Vcector2 m_Movement; // 이동 방향 벡터
 
-    public float Speed; // 이동 속도 
+    public float Speed; // 이동 속도
+
+    private Rigidbody2D rb; 
+
+    private Vector2 moveVelocity; // 이동 방향 벡터
+
 
     public float Jump = 5f; // 점프 힘 
 
@@ -17,27 +20,26 @@ public class Player : MonoBehaviour
     public bool IsDead = false; // 죽음 상태
 
 
-    private Vector2 moveVelocity; // 이동 방향 벡터
 
 
 
 
     private void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>(); // rigidbody2d 컴포넌트 가져오기
-        m_Movement = new Vector2(0, 0); // 이동 방향 초기화
+        rb = GetComponent<Rigidbody2D>(); // rigidbody2d 컴포넌트 가져오기
     }
 
     private void Update()
     {
-
-        transform.position += m_Movement * moveSpeed * Time.deltaTime; // 이동 처리
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); // 입력 값 가져오기
+        moveVelocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); // 이동 방향 벡터 설정
+        
 
     }
 
     private void FixedUpdate()
     {
-        velocity = new Vector2(moveVelocity.x, velocity.y); // 수평 이동 처리
+        moveVelocity = new Vector2(moveVelocity.x, moveVelocity.y); // 수평 이동 처리
     }
     
 
