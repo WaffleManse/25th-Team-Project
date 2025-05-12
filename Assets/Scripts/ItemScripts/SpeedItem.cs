4b1c046152376f690c9593cc18062ebeaf998c48
ChunBae20 <drchunbae@gmail.com>
2025-05-13 07:20:21 +0900

    [Feat][Fix] change item scripts
    [Feat][Fix] change item scripts

---
+++ Assets/Scripts/ItemScripts/SpeedItem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedItem : Itemcollision
+{
+
+    protected override void ExecuteFunction()
+    {
+        Debug.Log("스피드아이템획득");
+        // 일단 이거는 나중에 플레이어 컨트롤러랑 상의를 하자 스피드랑 듀레이션 어캐할지
+    }
+
+}
